78ba8241a728fee3f1dcb29ab6f7c617bca52d95
Ivan Belikov <belikov.workmail@gmail.com>
2025-04-17 18:47:43 +0300

    page titles added

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,11 +1,13 @@
 export const PUBLIC_ROUTES = {
   HOME: {
     PATH: '/',
+    TITLE: 'RTC | Sign in',
   },
 }
 
 export const AUTH_ROUTES = {
   HOME: {
     PATH: '/',
+    TITLE: 'RTC | Chat room',
   },
 }
